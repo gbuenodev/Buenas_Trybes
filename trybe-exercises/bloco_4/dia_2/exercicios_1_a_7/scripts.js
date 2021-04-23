f418f8a0fa37f42d15c3aaeb4d7ea0480cfbbfbc
Gabriel Bueno <gabrielsbueno@hotmail.com>
2021-04-23 11:55:13 -0300

    Exercicio 5 dia_2

--- trybe-exercises/bloco_4/dia_2/exercicios_1_a_7/scripts.js
+++ trybe-exercises/bloco_4/dia_2/exercicios_1_a_7/scripts.js
@@ -4,19 +4,29 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //     console.log(number);
 // };
 
-let resultado = 0;
+// let resultado = 0;
 
-for (let index = 0; index < numbers.length; index += 1) {
-    resultado = resultado + numbers[index];
-};
+// for (let index = 0; index < numbers.length; index += 1) {
+//     resultado += numbers[index];
+// };
 
-let media = resultado / numbers.length;
+// let media = resultado / numbers.length;
 
 // console.log(resultado);
 // console.log(media);
 
-if (media > 20) {
-    console.log('valor maior que 20');
-} else {
-    console.log('valor menor ou igual a 20');
-};
+// if (media > 20) {
+//     console.log('valor maior que 20');
+// } else {
+//     console.log('valor menor ou igual a 20');
+// };
+
+let maiorNumero = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if(numbers[index] > maiorNumero) {
+        maiorNumero = numbers[index];
+    }
+}
+
+console.log(maiorNumero);
