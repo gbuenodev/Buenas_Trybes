3ffcd431d9aa8657192bd3988af2bb0711d22961
Gabriel Bueno <gabrielsbueno@hotmail.com>
2021-04-23 11:33:41 -0300

    Exercício 2 dia_2

--- trybe-exercises/bloco_4/dia_2/exercicios_1_a_7/scripts.js
+++ trybe-exercises/bloco_4/dia_2/exercicios_1_a_7/scripts.js
@@ -1,6 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (number of numbers){
-    console.log(number)
+// for (number of numbers) {
+//     console.log(number);
+// };
+
+let resultado = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    resultado = resultado + numbers[index];
 };
+
+console.log(resultado);
     
