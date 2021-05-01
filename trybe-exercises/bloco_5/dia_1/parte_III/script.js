21afec3e9e32bdf2b83af663044f4b9fa0aac93d
Gabriel Bueno <gabrielsbueno@hotmail.com>
2021-05-01 16:15:04 -0300

    Exercícios da parte_III teórica do dia_1 bloco 5

---
+++ trybe-exercises/bloco_5/dia_1/parte_III/script.js
@@ -0,0 +1,25 @@
+let header = document.getElementById('header-container');
+header.style.color = 'white';
+header.style.backgroundColor = 'green';
+
+const trocaCor = (node, cor) => {
+  for (let index = 0; index < node.length; index += 1) {
+    node[index].style.backgroundColor = cor;
+  };
+};
+
+let section1 = document.getElementsByClassName('emergency-tasks');
+section1[0].style.backgroundColor = 'orange';
+
+let section2 = document.getElementsByClassName('no-emergency-tasks');
+section2[0].style.backgroundColor = 'yellow';
+
+let section1Title = document.querySelectorAll('.emergency-tasks div h3');
+trocaCor(section1Title, 'purple');
+
+let section2Title = document.querySelectorAll('.no-emergency-tasks div h3');
+trocaCor(section2Title, 'black');
+
+let footer = document.getElementById('footer-container');
+footer.style.backgroundColor = 'darkgreen';
+footer.style.marginTop = '5vh';
