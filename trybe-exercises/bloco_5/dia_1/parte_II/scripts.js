0e964b68846bab493eaccfa455434f0c7ff158e2
Gabriel Bueno <gabrielsbueno@hotmail.com>
2021-05-01 12:49:59 -0300

    Exercícios da parte teórica do dia_1 bloco 5

---
+++ trybe-exercises/bloco_5/dia_1/parte_II/scripts.js
@@ -0,0 +1,17 @@
+let paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+document.getElementById('page-title').innerText = 'Filé com Fritas'
+
+
+document.getElementById('second-paragraph').innerText = 'Rapaz. É bão. Filézin com fritas.Vai por mim.'
+
+document.getElementById('subtitle').innerText = 'É bão?'
+
+let paragraphs = document.getElementsByClassName('paragraph-class');
+
+paragraphs[0].style.backgroundColor = 'blue';
+
+let subtitle = document.getElementsByTagName('h4');
+
+subtitle[0].style.color = 'green';
